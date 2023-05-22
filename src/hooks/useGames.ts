60cee4519172f6834a58285d9ea429ff40b2e659
micha.seckar <michal.seckar@notino.com>
2023-05-22 20:38:45 +0200

    game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,18 +2,19 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-interface Games {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
   count: number;
-  results: Games[];
+  results: Game[];
 }
 
 const useGames = () => {
-  const [games, setGames] = useState<Games[]>([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
