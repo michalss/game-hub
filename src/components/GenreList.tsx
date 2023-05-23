b7085481a7ab70ec2e277fa7b8530b8c8b7d47cb
micha.seckar <michal.seckar@notino.com>
2023-05-23 10:30:39 +0200

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 export const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List spacing={2}>
       {data.map((dat) => (
-        <li key={dat.id}>{dat.name}</li>
+        <ListItem key={dat.id}>
+          <HStack spacing={2}>
+            <Image
+              borderRadius={8}
+              boxSize="32px"
+              src={getCroppedImageUrl(dat.image_background)}
+            ></Image>
+            <Text fontSize="lg">{dat.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
