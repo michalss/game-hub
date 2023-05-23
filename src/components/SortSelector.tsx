4b9aea4db595e1259883042b34e6b1be39446ccd
micha.seckar <michal.seckar@notino.com>
2023-05-23 17:39:42 +0200

    sortorder picker

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,38 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-export const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+export const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", lable: "Relevance" },
+    { value: "-added", lable: "Date added" },
+    { value: "name", lable: "Name" },
+    { value: "-released", lable: "Release data" },
+    { value: "-metacritic", lable: "Popularity" },
+    { value: "-rating", lable: "Average rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find((f) => f.value === sortOrder);
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order by: Relevance
+        Order by: {currentSortOrder?.lable || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>Date added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Release date</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average rating</MenuItem>
+        {sortOrders.map((sort) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(sort.value)}
+            key={sort.value}
+            value={sort.value}
+          >
+            {sort.lable}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
