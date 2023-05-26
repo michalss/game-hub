8b0a07e868a6bdbcce2076ea1410e935a70ecf96
micha.seckar <michal.seckar@notino.com>
2023-05-26 12:38:13 +0200

    remove unused code

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -10,32 +10,6 @@ const useData = <T>(
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
-
-  // useEffect(
-  //   () => {
-  //     const controller = new AbortController();
-
-  //     setLoading(true);
-  //     apiClient
-  //       .get<FetchResponse<T>>(endpoint, {
-  //         signal: controller.signal,
-  //         ...requestConfig,
-  //       })
-  //       .then((res) => {
-  //         setData(res.data.results);
-  //         setLoading(false);
-  //       })
-  //       .catch((err) => {
-  //         if (err instanceof CanceledError) return;
-  //         setError(err.message);
-  //         setLoading(false);
-  //       });
-
-  //     return () => controller.abort();
-  //   },
-  //   deps ? [...deps] : []
-  // );
-
   return { data, error, isLoading };
 };
 
